4eaa24e8ba3eea4f161ab9c0f4d627ccc7be052f
Jan Niklas Hasse <jhasse@gmail.com>
2012-04-06 13:52:09 +0200

    create Chipmunk++ Mac framework

---
+++ chipmunkpp/chipmunk++-mac/chipmunk___mac.h
@@ -0,0 +1,13 @@
+//
+//  chipmunk___mac.h
+//  chipmunk++-mac
+//
+//  Created by Jan Niklas Hasse on 03.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface chipmunk___mac : NSObject
+
+@end
---
+++ chipmunkpp/chipmunk++-mac/chipmunk___mac.m
@@ -0,0 +1,13 @@
+//
+//  chipmunk___mac.m
+//  chipmunk++-mac
+//
+//  Created by Jan Niklas Hasse on 03.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "chipmunk___mac.h"
+
+@implementation chipmunk___mac
+
+@end
