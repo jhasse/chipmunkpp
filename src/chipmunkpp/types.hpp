8f1261663d5a844a2edb09ce7291e9acd9de6bf3
Jan Niklas Hasse <jhasse@gmail.com>
2013-09-09 22:50:31 +0200

    Visual C++ 2013 now supports explicit conversion operators.

--- src/chipmunkpp/types.hpp
+++ src/chipmunkpp/types.hpp
@@ -16,11 +16,7 @@ namespace cp {
 		Layers(unsigned int layerNr); // one layer
 		Layers operator~() const;
 		Layers operator|(Layers rhs) const;
-#ifdef _MSC_VER
-		operator cpLayers() const;
-#else
 		explicit operator cpLayers() const;
-#endif
 	private:
 		cpLayers l;
 	};
@@ -29,11 +25,7 @@ namespace cp {
 	class Group {
 	public:
 		Group(cpGroup);
-#ifdef _MSC_VER
-		operator cpGroup() const;
-#else
 		explicit operator cpGroup() const;
-#endif
 	private:
 		cpGroup g;
 	};
