34914f21da2f97ffc5c5d9fa913ca0a03a9dab11
Jan Niklas Hasse <jhasse@bixense.com>
2019-03-20 18:54:24 +0100

    clang-format on types.hpp and vect.hpp

--- src/chipmunkpp/types.hpp
+++ src/chipmunkpp/types.hpp
@@ -3,28 +3,30 @@
 #include <chipmunk.h>
 
 namespace cp {
-	typedef cpFloat Float;
-	typedef cpCollisionType CollisionType;
-	typedef cpDataPointer DataPointer;
+typedef cpFloat Float;
+typedef cpCollisionType CollisionType;
+typedef cpDataPointer DataPointer;
 
-	class Layers {
-	public:
-		Layers(); // no layers
-		Layers(unsigned int layerNr); // one layer
-		Layers operator~() const;
-		Layers operator|(Layers rhs) const;
-		explicit operator cpBitmask() const;
-	private:
-		cpBitmask l;
-	};
-	const static Layers ALL_LAYERS(~(Layers()));
+class Layers {
+public:
+	Layers();                     // no layers
+	Layers(unsigned int layerNr); // one layer
+	Layers operator~() const;
+	Layers operator|(Layers rhs) const;
+	explicit operator cpBitmask() const;
 
-	class Group {
-	public:
-		Group(cpGroup);
-		explicit operator cpGroup() const;
-	private:
-		cpGroup g;
-	};
-	const static Group NO_GROUP(CP_NO_GROUP);
-}
+private:
+	cpBitmask l;
+};
+const static Layers ALL_LAYERS(~(Layers()));
+
+class Group {
+public:
+	Group(cpGroup);
+	explicit operator cpGroup() const;
+
+private:
+	cpGroup g;
+};
+const static Group NO_GROUP(CP_NO_GROUP);
+} // namespace cp
