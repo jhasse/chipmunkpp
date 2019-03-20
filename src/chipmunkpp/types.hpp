0baa56aebf28a5eb323af00d33bc3f21a944ac79
Jan Niklas Hasse <jhasse@bixense.com>
2019-03-20 18:54:24 +0100

    clang-format on types.hpp and vect.hpp

--- src/chipmunkpp/types.hpp
+++ src/chipmunkpp/types.hpp
@@ -3,28 +3,32 @@
 #include <chipmunk.h>
 
 namespace cp {
-	typedef cpFloat Float;
-	typedef cpCollisionType CollisionType;
-	typedef cpDataPointer DataPointer;
 
-	class Layers {
-	public:
-		Layers(); // no layers
-		Layers(unsigned int layerNr); // one layer
-		Layers operator~() const;
-		Layers operator|(Layers rhs) const;
-		explicit operator cpLayers() const;
-	private:
-		cpLayers l;
-	};
-	const static Layers ALL_LAYERS(~(Layers()));
+using Float = cpFloat;
+using CollisionType = cpCollisionType;
+using DataPointer = cpDataPointer;
 
-	class Group {
-	public:
-		Group(cpGroup);
-		explicit operator cpGroup() const;
-	private:
-		cpGroup g;
-	};
-	const static Group NO_GROUP(CP_NO_GROUP);
-}
+class Layers {
+public:
+	Layers();                              // no layers
+	explicit Layers(unsigned int layerNr); // one layer
+	Layers operator~() const;
+	Layers operator|(Layers rhs) const;
+	explicit operator cpLayers() const;
+
+private:
+	cpLayers l;
+};
+const static Layers ALL_LAYERS(~(Layers()));
+
+class Group {
+public:
+	Group(cpGroup);
+	explicit operator cpGroup() const;
+
+private:
+	cpGroup g;
+};
+const static Group NO_GROUP(CP_NO_GROUP);
+
+} // namespace cp
--- src/chipmunkpp/vect.hpp
+++ src/chipmunkpp/vect.hpp
@@ -5,114 +5,118 @@
 #include <iosfwd>
 
 namespace cp {
-	/// 2D Vector class
-	class Vect {
-	public:
-		/// Constructor
-		Vect(Float x, Float y);
 
-		/// Creates the zero vector.
-		Vect();
+/// 2D Vector class
+class Vect {
+public:
+	/// Constructor
+	Vect(Float x, Float y);
 
-		/// Copy constructor
-		Vect(const cpVect&);
+	/// Creates the zero vector.
+	Vect();
 
-		/// Conversion operator to Chipmunk's cpVect.
-		operator cpVect() const;
+	/// Copy constructor
+	Vect(const cpVect&);
 
-		Vect& operator/=(const Float v);
-		Vect& operator*=(const Float v);
-		Vect& operator-=(const Vect& rhs);
-		Vect& operator-=(const Float v);
-		Vect& operator+=(const Float v);
-		Vect& operator+=(const Vect& rhs);
-		Vect& operator++();
-		Vect& operator--();
+	/// Conversion operator to Chipmunk's cpVect.
+	operator cpVect() const;
 
-		/// Returns the length.
-		Float length() const;
+	Vect& operator/=(Float v);
+	Vect& operator*=(Float v);
+	Vect& operator-=(const Vect& rhs);
+	Vect& operator-=(Float v);
+	Vect& operator+=(Float v);
+	Vect& operator+=(const Vect& rhs);
+	Vect& operator++();
+	Vect& operator--();
 
-		/// Returns the squared length. Faster than Vect::length() when you only need to compare lengths.
-		Float lengthSq() const;
+	/// Returns the length.
+	Float length() const;
 
-		/// Returns a perpendicular vector. (90 degree rotation)
-		Vect perp() const;
+	/// Returns the squared length. Faster than Vect::length() when you only need to compare
+	/// lengths.
+	Float lengthSq() const;
 
-		/// Returns a perpendicular vector. (-90 degree rotation)
-		Vect rperp() const;
+	/// Returns a perpendicular vector. (90 degree rotation)
+	Vect perp() const;
 
-		/// Returns a normalized copy.
-		Vect normalize() const;
+	/// Returns a perpendicular vector. (-90 degree rotation)
+	Vect rperp() const;
 
-		/// Returns a normalized copy or the zero vector if the vector was already the zero vector. Protects against
-		/// divide by zero errors.
-		Vect normalizeSafe() const;
+	/// Returns a normalized copy.
+	Vect normalize() const;
 
-		/// Clamp v to length len.
-		Vect clamp(Float len) const;
+	/// Returns a normalized copy or the zero vector if the vector was already the zero vector.
+	/// Protects against divide by zero errors.
+	Vect normalizeSafe() const;
 
-		/// Returns the angular direction the vector is pointing in (in radians).
-		Float toAngle() const;
+	/// Clamp v to length len.
+	Vect clamp(Float len) const;
 
-		/// Uses complex multiplication to return a copy rotated by v. Scaling will occur if the vector isn't a unit vector.
-		Vect rotate(Vect v) const;
+	/// Returns the angular direction the vector is pointing in (in radians).
+	Float toAngle() const;
 
-		/// Inverse of Vect::rotate.
-		Vect unrotate(Vect v) const;
+	/// Uses complex multiplication to return a copy rotated by v. Scaling will occur if the vector
+	/// isn't a unit vector.
+	Vect rotate(Vect v) const;
 
-		/// x component
-		Float x;
+	/// Inverse of Vect::rotate.
+	Vect unrotate(Vect v) const;
 
-		/// y component
-		Float y;
+	/// x component
+	Float x;
 
-		/// Returns the unit length vector for the given angle (in radians).
-		static Vect forAngle(Float a);
+	/// y component
+	Float y;
 
-		/// Vector dot product.
-		static Float dot(Vect v1, Vect v2);
+	/// Returns the unit length vector for the given angle (in radians).
+	static Vect forAngle(Float a);
 
-		/// 2D vector cross product analog. The cross product of 2D vectors results in a 3D vector with only a z
-		/// component. This function returns the value along the z-axis.
-		static Float cross(Vect v1, Vect v2);
+	/// Vector dot product.
+	static Float dot(Vect v1, Vect v2);
 
-		/// Returns the vector projection of v1 onto v2.
-		static Vect project(Vect v1, Vect v2);
+	/// 2D vector cross product analog. The cross product of 2D vectors results in a 3D vector with
+	/// only a z component. This function returns the value along the z-axis.
+	static Float cross(Vect v1, Vect v2);
 
-		/// Linearly interpolate between a and b.
-		static Vect lerp(Vect v1, Vect v2, Float t);
+	/// Returns the vector projection of v1 onto v2.
+	static Vect project(Vect v1, Vect v2);
 
-		/// Linearly interpolate between v1 towards v2 by distance d.
-		static Vect lerpconst(Vect v1, Vect v2, Float d);
+	/// Linearly interpolate between a and b.
+	static Vect lerp(Vect v1, Vect v2, Float t);
 
-		/// Spherical linearly interpolate between v1 and v2.
-		static Vect slerp(Vect v1, Vect v2, Float t);
+	/// Linearly interpolate between v1 towards v2 by distance d.
+	static Vect lerpconst(Vect v1, Vect v2, Float d);
 
-		/// Spherical linearly interpolate between v1 towards v2 by no more than angle a in radians.
-		static Vect slerpconst(Vect v1, Vect v2, Float a);
+	/// Spherical linearly interpolate between v1 and v2.
+	static Vect slerp(Vect v1, Vect v2, Float t);
 
-		/// Returns the distance between v1 and v2.
-		static Float dist(Vect v1, Vect v2);
+	/// Spherical linearly interpolate between v1 towards v2 by no more than angle a in radians.
+	static Vect slerpconst(Vect v1, Vect v2, Float a);
 
-		/// Returns the squared distance between v1 and v2. Faster than Vect::dist when you only need to compare
-		/// distances.
-		static Float distSq(Vect v1, Vect v2);
+	/// Returns the distance between v1 and v2.
+	static Float dist(Vect v1, Vect v2);
 
-		/// Returns true if the distance between v1 and v2 is less than dist.
-		static bool near(Vect v1, Vect v2, Float dist);
-	};
+	/// Returns the squared distance between v1 and v2. Faster than Vect::dist when you only need to
+	/// compare distances.
+	static Float distSq(Vect v1, Vect v2);
 
-	bool operator==(const Vect& lhs, const Vect& rhs);
-	bool operator!=(const Vect& lhs, const Vect& rhs);
-	Vect operator*(const Vect& lhs, const Float v);
-	Vect operator/(const Vect& lhs, const Float v);
-	Vect operator*(const Float v, const Vect& rhs);
-	Vect operator/(const Float v, const Vect& rhs);
-	Float operator*(const Vect& lhs, const Vect& rhs);
-	Vect operator-(const Vect& lhs, const Vect& rhs);
-	Vect operator+(const Vect& lhs, const Vect& rhs);
-	bool operator<(const Vect& lhs, const Vect& rhs);
-	bool operator>(const Vect& lhs, const Vect& rhs);
-}
+	/// Returns true if the distance between v1 and v2 is less than dist.
+	static bool near(Vect v1, Vect v2, Float dist);
+};
+
+bool operator==(const Vect& lhs, const Vect& rhs);
+bool operator!=(const Vect& lhs, const Vect& rhs);
+Vect operator*(const Vect& lhs, Float v);
+Vect operator/(const Vect& lhs, Float v);
+Vect operator*(Float v, const Vect& rhs);
+Vect operator/(Float v, const Vect& rhs);
+Float operator*(const Vect& lhs, const Vect& rhs);
+Vect operator-(const Vect& lhs, const Vect& rhs);
+Vect operator+(const Vect& lhs, const Vect& rhs);
+bool operator<(const Vect& lhs, const Vect& rhs);
+bool operator>(const Vect& lhs, const Vect& rhs);
+
+} // namespace cp
 
 std::ostream& operator<<(std::ostream&, const cp::Vect&);
